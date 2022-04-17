c1de77bbadfaac338e0f1e084fdba48db4f67da9
洮羱芝闇 <shun_@outlook.com>
2022-04-17 08:49:19 +0800

    update

---
+++ pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<view class="top"></view>
+		<view class="swiper-c">
+					<swiper indicator-dots="true" autoplay="autoplay" interval="1000" duration="500">
+						<swiper-item>
+							<image src="https://s2.loli.net/2022/04/16/Cs7fcykHFMipUjE.png"></image>
+						</swiper-item>
+						<swiper-item>
+							<image src="https://s2.loli.net/2022/04/16/gLOiyTqZFrdGYR6.png"></image>
+						</swiper-item>
+						<swiper-item>
+							<image src="https://s2.loli.net/2022/04/16/fEJvxtRjIdzsLU5.png"></image>
+						</swiper-item>
+						<swiper-item>
+							<image src=""></image>
+						</swiper-item>
+						<swiper-item>
+							<image src=""></image>
+						</swiper-item>
+					</swiper>
+				</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.top {
+			height: var(--status-bar-height);
+			width: 100%;
+			background-color: #FFFFFF;
+		}
+	.swiper-c{
+			width: 100%;
+			height: 400rpx;
+		}
+		.swiper-c swiper{
+			width: 100%;
+			height: 100%;
+		}
+		.swiper-c swiper swiper-item image{
+			width: 100%;
+			height: 100%;
+		}
+</style>
